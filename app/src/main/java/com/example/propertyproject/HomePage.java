7c3fcd6ded7113ba2312f361f13c9845887eb9d5
Damianglu <damian.gluszkiewicz123@gmail.com>
2022-03-30 13:14:39 +0100

    Added onClickListeners for the buttons on the home page

--- app/src/main/java/com/example/propertyproject/HomePage.java
+++ app/src/main/java/com/example/propertyproject/HomePage.java
@@ -12,7 +12,11 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.propertyproject.chart.ContractorChartActivity;
+import com.example.propertyproject.chart.RentChartActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity {
@@ -20,56 +24,45 @@ public class HomePage extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+    Button mpropertyHome, mcontractorHome, mrentStatsHome, mcontractorStatsHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-                // drawer layout instance to toggle the menu icon to open
-                // drawer and back button to close drawer
-                drawerLayout = findViewById(R.id.my_drawer_layout);
-                actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-                firebaseAuth = FirebaseAuth.getInstance();
+        mpropertyHome = findViewById(R.id.propertyHome);
+        mcontractorHome = findViewById(R.id.contractorHome);
+        mrentStatsHome = findViewById(R.id.rentStatsHome);
+        mcontractorStatsHome = findViewById(R.id.contractorStatsHome);
 
-
-        // pass the Open and Close toggle for the drawer layout listener
-                // to toggle the button
-                drawerLayout.addDrawerListener(actionBarDrawerToggle);
-                actionBarDrawerToggle.syncState();
-
-                // to make the Navigation drawer icon always appear on the action bar
-                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.PropertyMenu:
+        mpropertyHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 startActivity(new Intent(HomePage.this, PropertyActivity.class));
-                return true;
-            case R.id.ContractorMenu:
+            }
+        });
+
+        mcontractorHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 startActivity(new Intent(HomePage.this, contractorActivity.class));
-                return true;
-            case R.id.CovidMenu:
-                startActivity(new Intent(HomePage.this, C19Tracker.class));
-                return true;
-            case R.id.WeatherMenu:
-                startActivity(new Intent(HomePage.this, WeatherActivity.class));
-                return true;
-            case R.id.MapsMenu:
-                startActivity(new Intent(HomePage.this, MapsActivity.class));
-                return true;
-            case R.id.logout:
-                firebaseAuth.signOut();
-                finish();
-                startActivity(new Intent(HomePage.this, MainActivity.class));
-                return true;
-            default: return super.onOptionsItemSelected(item);
-        }
+            }
+        });
+
+        mrentStatsHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomePage.this, RentChartActivity.class));
+            }
+        });
+
+        mcontractorStatsHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomePage.this, ContractorChartActivity.class));
+            }
+        });
 
     }
-
-        }
+}
